e23b7a115dc7856634b91916c1c890df4cf63579
kirico <kirico1027@icloud.com>
2024-01-12 17:56:02 +0900

    全ページ・コーディング完了(仮)

--- src/js/script.js
+++ src/js/script.js
@@ -148,4 +148,12 @@ $(function () {
   });
 });
 
+// faq
+jQuery(function ($) {
+  $('.js-faq-question').on('click', function () {
+      $(this).next().slideToggle();
+      $(this).toggleClass('is-open');
+  });
+});
+
 });
